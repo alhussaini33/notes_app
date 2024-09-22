bae9e2fade28a3fc2968e9eb1da94b728d6a466a
Alhussaini Mahmoud <malhussaini331@gmail.com>
2024-09-23 01:11:13 +0300

    make status bar same as body color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:note_app/views/notes_view.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class NotesaApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Colors.transparent);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
