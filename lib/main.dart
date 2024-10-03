5ae8e098b38810295e3b81cea80552844808b94a
Alhussaini Mahmoud <malhussaini331@gmail.com>
2024-10-03 19:05:37 +0300

    inite Hive & open box & and create file for const variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/constants/variables.dart';
 import 'package:note_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(Variables.noteBox);
   runApp(const NotesaApp());
 }
 
